bc19ab398ff2e3cbd4fdfeb2d79648b728f4f56d
Pawel Wier <pawiercinski@gmail.com>
2021-07-23 16:28:40 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FormatInput input-title="Second" />
+    <FormatInput input-title="Minute" />
+    <FormatInput input-title="Hour" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormatInput from './components/FormatInput.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FormatInput
   }
 }
 </script>
@@ -21,8 +22,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
 </style>
---
+++ src/components/FormatInput.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+   <h2>
+    {{ inputTitle }}
+   </h2>
+    <div class="input-wrapper" @click="setMethod(0)">
+      <div>
+        Every {{ unit }}
+      </div>
+    </div>
+    <div class="input-wrapper" @click="setMethod(1)">
+      <div>
+        Every
+      </div>
+      <div>
+        <input type="text">
+      </div>
+      <div>
+        {{ unit }}(s)
+      </div>
+    </div>
+    <div class="input-wrapper" @click="setMethod(2)">
+      <div>
+        Specific {{ unit }}(s) (divided by comma ',')
+      </div>
+      <div>
+        <input type="text">
+      </div>
+    </div>
+    <div class="input-wrapper" @click="setMethod(3)">
+      <div>
+        Every {{ unit }} between
+      </div>
+      <div>
+        <input type="text">
+      </div>
+      <div>
+        and
+      </div>
+      <div>
+        <input type="text">
+      </div>
+    </div>
+    <div class="result">
+      Result: {{ result }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormatInput",
+  props: {
+    inputTitle: {
+      required: true,
+      type: String,
+    }
+  },
+  data() {
+    return {
+      everyUnit: '',
+      specificUnit: '',
+      betweenUnitOne: '',
+      betweenUnitTwo: '',
+    }
+  },
+  computed:{
+    unit() {
+      return this.inputTitle.toLowerCase()
+    },
+    result() {
+      return 'x'
+    },
+  },
+  methods: {
+    setMethod(param) {
+      console.log(param)
+    },
+  }
+}
+</script>
+
+<style scoped>
+  .input-wrapper {
+    display: flex;
+    column-gap: 6px;
+  }
+
+  .input-wrapper:hover {
+    cursor: pointer;
+  }
+
+  .result {
+    padding-top: 10px;
+  }
+
+  input[type=text] {
+    width: 70px;
+  }
+</style>
